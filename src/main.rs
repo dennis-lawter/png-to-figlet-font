8cf091524b92818ad12ff89ca1a2b71d69892569
bytomancer <bytomancer@gmail.com>
2024-05-14 19:00:20 -0400

    Output working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Write};
+
 use clap::{command, Parser};
 
 use color_eyre::{eyre::eyre, Result};
@@ -27,36 +29,75 @@ struct Args {
     output: String,
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 struct FigletGlyph {
     rows: Vec<String>,
 }
 impl FigletGlyph {
-    fn new(w: u32, h: u32) -> Self {
-        let blank_string = " ".repeat(w as usize);
+    fn new(h: u32) -> Self {
+        // let blank_string = " ".repeat(w as usize);
+        let blank_string = String::from("");
         let rows: Vec<String> = vec![blank_string.clone(); h as usize];
+        // let rows = Vec::with_capacity(h as usize);
 
         Self { rows }
     }
+
+    fn output(&self) -> String {
+        let mut out = String::new();
+        for i in 0..self.rows.len() {
+            out.push_str(self.rows[i].as_str());
+            if i == self.rows.len() - 1 {
+                out.push_str("@@\n");
+            } else {
+                out.push_str("@\n");
+            }
+        }
+        out
+    }
 }
 
+#[derive(Debug, Clone)]
 struct FigletFont {
-    char_width: u32,
+    // char_width: u32,
     char_height: u32,
     glyphs: Vec<FigletGlyph>,
 }
 impl FigletFont {
-    fn new(w: u32, h: u32) -> Self {
-        let blank_glyph = FigletGlyph::new(w, h);
+    fn new(h: u32) -> Self {
+        let blank_glyph = FigletGlyph::new(h);
         Self {
-            char_width: w,
+            // char_width: w,
             char_height: h,
             glyphs: vec![blank_glyph; 6 * 16],
         }
     }
+    fn output(&self) -> String {
+        let header = format!("flf2a$ {} {} 20 -1 2\n", self.char_height, self.char_height);
+        let mut out = String::from(header);
+        out.push_str("Font automatically generated from rust crate png-to-figlet-font\n\n");
+
+        for glyph in &self.glyphs {
+            out.push_str(glyph.output().as_str());
+        }
+
+        let space_glyph = &self.glyphs[0];
+        for _ in 0..6 {
+            out.push_str(space_glyph.output().as_str());
+        }
+
+        out
+    }
 }
 
-const CHAR_PIXEL: char = '█';
+// const PIXEL_CHAR: char = '█';
+// const PIXEL_CHAR: char = '▚';
+const PIXEL_CHAR: char = '▉';
+// const PIXEL_CHAR: char = '▇'; // 7/8
+
+// const PIXEL_CHAR: char = '■';
+
+const BLANK_CHAR: char = ' ';
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -78,30 +119,39 @@ fn main() -> Result<()> {
     let char_width = width / 16;
     let char_height = height / 6;
 
-    let mut font = FigletFont::new(char_width, char_height);
+    println!("Detected character size: {} x {}", char_width, char_height);
+
+    let mut font = FigletFont::new(char_height);
+
+    let mut debug_coords: Vec<(u32, u32)> = vec![];
 
     for glyph_y in 0u32..6 {
         for glyph_x in 0u32..16 {
             let glyph_index = glyph_y * 16 + glyph_x;
+            let glyph = &mut font.glyphs[glyph_index as usize];
             for local_y in 0u32..char_height {
+                let relevant_string = &mut glyph.rows[local_y as usize];
                 for local_x in 0u32..char_width {
-                    let x = glyph_x * 16 + local_x;
-                    let y = glyph_y * 6 + local_y;
+                    let x = glyph_x * char_width + local_x;
+                    let y = glyph_y * char_height + local_y;
+                    debug_coords.push((x, y));
 
                     let pixel = image.get_pixel(x, y);
                     let luminance = pixel.to_luma().0[0];
                     if luminance != 0 {
-                        let glyph = &mut font.glyphs[glyph_index as usize];
-                        let relevant_string = &mut glyph.rows[local_y as usize];
-                        relevant_string.replace_range(
-                            (local_x as usize)..=(local_x as usize),
-                            &String::from(CHAR_PIXEL),
-                        );
+                        relevant_string.push(PIXEL_CHAR);
+                    } else {
+                        relevant_string.push(BLANK_CHAR);
                     }
                 }
             }
         }
     }
+
+    // println!("{:?}", debug_coords);
+
+    let mut file = File::create(&args.output)?;
+    file.write_all(font.output().as_bytes())?;
 
     Ok(())
 }
